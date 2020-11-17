0ac28345cde959b3ab34b7a831b68bb90a7dc3ed
82103 <82103@DESKTOP-BM23VCQ>
2020-11-17 20:00:17 +0900

    수정

--- src/main/java/org/edu/contoller/HomeController.java
+++ src/main/java/org/edu/contoller/HomeController.java
@@ -40,6 +40,7 @@ public class HomeController {
 		String formattedDate = dateFormat.format(date);//변수가 실행됩니다.
 		//위 변수가 실행되어서 출력된 결과값이 string클래스형 변수 formattedDate변수에 담기게 됩니다.
 		model.addAttribute("TomcatserverTime", formattedDate );//serverTime 변수값으로 jsp파일로 이동.
+		System.out.println("현재서버의시간은" + formattedDate + "입니다.");
 		//위 mode1이라는 클래스형 변수를이용해서, severTime변수값을 아래 home (생략.Jsp)로 전송해
 		return "home";//결과적으로 return 출력값이 home(생략.jsp)에 연동됩니다.
 	}
