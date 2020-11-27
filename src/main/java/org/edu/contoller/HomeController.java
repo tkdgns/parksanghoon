c2e627c7b9a1abbae78a3cd4eec0c55c429c2716
82103 <82103@DESKTOP-BM23VCQ>
2020-11-27 16:57:13 +0900

    커밋

--- src/main/java/org/edu/contoller/HomeController.java
+++ src/main/java/org/edu/contoller/HomeController.java
@@ -26,7 +26,12 @@ public class HomeController {
 	 * 그래서, 저장시 콜솔에 보시면, Reloading context with...is completed 재실행 되었다고 나옵니다.
 	 * 그래서, java 클래스는 수정 후 약간 기다린 후 Reloading.. 메시지 후 결과화인이 가능합니다.
 	 */
-	@RequestMapping(value="/contack",method=RequestMethod.GET)
+	@RequestMapping(value="/contact",method=RequestMethod.POST)//포스트방식은 테이터를 전송하는 역활
+	public String contact_send() {
+		//데이터 전송후에 다른 페이지 이동이 필요합니다. 새로고침을 방지하기위해서.
+		return "redirect:/blog";
+	}
+	@RequestMapping(value="/contack",method=RequestMethod.GET)//겟방식은 폼페이지를 보여주는 역할
 	public String contact() {
 		return "sample/contact";
 	}
--- src/main/webapp/WEB-INF/views/sample/contact.jsp
+++ src/main/webapp/WEB-INF/views/sample/contact.jsp
@@ -8,7 +8,7 @@
 <div class="container">
 <!-- 폼태그의 필수 속성, name, action-데이터를 전송할 대상값, method-데이터를 전송하는 방법값 -->
 <!-- input 한줄입력 태그, textarea 여러줄입력 태그 -->
-<form name="message_form" action="contact.html" method="post">
+<form name="message_form" action="/contact" method="post">
 <!-- 플케이스 홀더:공간에 자리를 차지하지 않으면서 입력힌트를 보여주는 역활 -->
 <fieldset>
 <legend>현재폼이타이틀영역입니다.</legend>
